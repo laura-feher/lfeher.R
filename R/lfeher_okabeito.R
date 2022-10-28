69354f05ea0c72cf0fe29a08c0a1af6b6a0c8de6
Feher <lfeher@usgs.gov>
2022-10-28 12:17:01 -0500

    🎉 initial commit

---
+++ R/lfeher_okabeito.R
@@ -0,0 +1,15 @@
+#' Hex codes for Okabe-Ito colors
+#'
+#' Hex codes for Okabe-Ito colors.
+#' @param base_color base color. Defaults to black.
+#' @keywords lfeher
+#' @export
+#' @examples
+#' lfeher_okabeito()
+#'
+lfeher_okabeito <- function(base_color = "#000000") {
+  list(color = c("black", "orange", "sky blue", "blueish green",
+                 "yellow", "blue", "vermillion", "reddish purple"),
+       hex_code = c(base_color, "#E69F00", "#56B4E9", "#009E73",
+                    "#F0E442", "#0072B2", "#D55E00", "#CC79A7"))
+}
---
+++ R/lfeher_theme.R
@@ -0,0 +1,35 @@
+#' Preferred ggplot theme
+#'
+#' Preferred ggplot theme.
+#' @param base_size base size. Defaults to 10.
+#' @param base_color base color. Defaults to black.
+#' @keywords lfeher
+#' @export
+#' @examples
+#' lfeher_theme()
+#'
+lfeher_theme <- function(base_size = 10, base_color = "black") {
+  ggplot2::theme(
+    text = ggplot2::element_text(size = base_size, color = base_color),
+    panel.grid = ggplot2::element_blank(),
+    panel.border = ggplot2::element_rect(fill = NA, color = base_color),
+    panel.background = ggplot2::element_rect(fill = NA, color = base_color),
+
+    axis.text.x.top = ggplot2::element_blank(),
+    axis.text.x.bottom = ggplot2::element_text(size = base_size, color = base_color),
+
+    axis.text.y.right = ggplot2::element_blank(),
+    axis.text.y.left = ggplot2::element_text(size = base_size, color = base_color),
+
+    axis.title.x.top = ggplot2::element_blank(),
+    axis.title.x.bottom = ggplot2::element_text(size = base_size, color = base_color),
+
+    axis.title.y.right = ggplot2::element_blank(),
+    axis.title.y.left = ggplot2::element_text(size = base_size, color = base_color),
+
+    axis.ticks.length = ggplot2::unit(-2.75, "pt"),
+    legend.text = ggplot2::element_text(size = base_size, color = base_color),
+    legend.title = ggplot2::element_text(size = base_size, color = base_color),
+    complete = FALSE
+  )
+}
